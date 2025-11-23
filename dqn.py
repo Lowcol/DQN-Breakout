08663802e7d1ba04cfe3ffa64cdd3534d5f15a4e
godin <antoine.godin.1@ens.etsmtl.ca>
2025-11-23 13:30:40 -0500

    2025-11-23

--- dqn.py
+++ dqn.py
@@ -9,36 +9,38 @@ class DQN(nn.Module):
         
         self.enable_dueling_dqn = enable_dueling_dqn
         
-        # convolutional layers
-        self.conv1 = nn.Conv2d(in_channels=4, out_channels=16, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2)
+        # convolutional layers (DeepMind architecture)
+        self.conv1 = nn.Conv2d(in_channels=4, out_channels=32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
         
-        # fully connected layers
-        self.flatten_size = 32*9*9
+        # fully connected layers (7x7 after 3 conv layers)
+        self.flatten_size = 64*7*7  # 3136
         
         if self.enable_dueling_dqn:
             # value stream
-            self.fc_value = nn.Linear(self.flatten_size, 256)
-            self.value = nn.Linear(256, 1)
+            self.fc_value = nn.Linear(self.flatten_size, 512)
+            self.value = nn.Linear(512, 1)
             
             # advantage stream
-            self.fc_advantage = nn.Linear(self.flatten_size, 256)
-            self.advantage = nn.Linear(256, action_dim)
+            self.fc_advantage = nn.Linear(self.flatten_size, 512)
+            self.advantage = nn.Linear(512, action_dim)
             
         else:
             # standard DQN
-            self.fc1 = nn.Linear(self.flatten_size, action_dim)
-            self.fc2 = nn.Linear(256, action_dim)
+            self.fc1 = nn.Linear(self.flatten_size, 512)
+            self.fc2 = nn.Linear(512, action_dim)
     
     def forward(self, x):
         # normalize pixel values from [0, 255] to [0, 1]
         x=x/255.0
         
-        # convolutional feature extraction
+        # convolutional feature extraction (3 layers as per DeepMind)
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
         
-        # flatten: (batch, 32, 9, 9) -> (batch, 32*9*9)
+        # flatten: (batch, 64, 7, 7) -> (batch, 64*7*7)
         x = x.view(x.size(0), -1) # flatten
         
         if self.enable_dueling_dqn:
